ab955d8e7d6de6601747c71bc0fd441d2e5cbece
Md. Sadman Rahman <sadmanrman@gmail.com>
2021-02-07 12:36:33 +0600

    Added 2 other projects

---
+++ HelloWorld/EvenOddSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int evenSum = 0;
+            int oddSum = 0;
+
+
+            for (int num = 0; num < 100; num++)
+            {
+                if (num % 2 == 0)
+                {
+                    evenSum = evenSum + num;
+                }
+                else
+                {
+                    oddSum = oddSum + num;
+                }
+            }
+            Console.WriteLine("Sum of even numbers : " + evenSum); 
+            Console.WriteLine("Sum of all odd numbers : " + oddSum);
+        }
+    }
+}
---
+++ HelloWorld/GradeReport/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Physics = 88;
+            int Chemistry = 78;
+            int Biology = 75;
+            int Mathematics = 86;
+            int Computer = 92;
+
+            int average = (Physics + Chemistry + Biology + Mathematics + Computer) / 5;
+
+
+        if (average>50 && average < 75){
+                Console.Write("Grade C+");
+            }
+        if (average > 75 && average < 79)
+            {
+                Console.Write("Grade B");
+            }
+        if (average > 80 && average < 85)
+            {
+                Console.Write("Grade B+");
+            }
+        if (average > 85 && average < 89)
+            {
+                Console.Write("Grade A");
+            }
+            if (average >90)
+            {
+                Console.Write("Grade A");
+            }
+        }
+    }
+}
