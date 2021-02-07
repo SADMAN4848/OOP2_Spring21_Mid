f2e624f81e477a8335946ac659be84461c941c2f
Md. Sadman Rahman <sadmanrman@gmail.com>
2021-02-07 11:59:25 +0600

    First upload

---
+++ HelloWorld/EvenPrint/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            Console.WriteLine("Even Numbers :");
+            for (i = 1; i <= 20; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.Write(i + ", ");
+                }
+            }
+
+            
+        }
+    }
+}
