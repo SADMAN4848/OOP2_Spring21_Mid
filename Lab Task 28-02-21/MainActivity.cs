62b8ed3896764788f422a88b933f50d8812296ae
Md. Sadman Rahman <sadmanrman@gmail.com>
2021-02-28 14:07:32 +0600

    Completed class task

---
+++ Lab Task 28-02-21/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_28_02_21
+{
+    class MainActivity
+    {
+        static void Main(string[] args)
+        {
+            Department d1 = new Department("Computer Science","CS");
+            Department d2 = new Department("Electrical Engineering", "EEE");
+            Student s1 = new Student("Sadman Rahman", "123", 2.5f);
+            Student s2 = new Student("Mehrab Faisal", "124", 2.75f);
+            Student s3 = new Student("Rashedul Hasan", "126", 3.3f);
+
+            d1.AddStudent(s1,s2);
+            s1.Department = d1;
+            s2.Department = d1;
+            s1.Department.ShowInfo();
+            
+            d2.AddStudent(s3);
+            s3.Department = d2;
+            
+
+            d1.AllStudents();
+            d2.AllStudents();
+
+ 
+        }
+    }
+}
---
+++ Lab Task 28-02-21/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_28_02_21
+{
+    class Student
+    {
+        private string name;
+        
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string id;
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private float cgpa;
+        public float Cgpa
+        {
+            get { return cgpa; }
+            set { cgpa = value; }
+        }
+
+        Department department;
+        public Department Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+        public Student() { }
+        public Student(string name, string id, float cgpa)
+        {
+            this.name = name;
+            this.id = id;
+            this.cgpa = cgpa;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Id: " + id);
+            Console.WriteLine("Cgpa: " + cgpa);
+        }
+        
+    }
+}
