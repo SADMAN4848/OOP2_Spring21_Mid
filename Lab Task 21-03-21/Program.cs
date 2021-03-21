45844f6882429001119844888d6f483e949ae420
Md. Sadman Rahman <sadmanrman@gmail.com>
2021-03-21 14:04:33 +0600

    Started project

---
+++ Lab Task 21-03-21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_21_03_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account("Sadman", "22345", 500);
+            Account a2 = new Account("Sakib", "22346", 250);
+            Transaction t1 = new Transaction(a1,a2,200, "Credited");
+        }
+    }
+}
---
+++ Lab Task 21-03-21/Transactions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_21_03_21
+{
+    public class Transaction
+    {
+        Account sender;
+        Account receiver;
+        private int amount;
+        private string additionalInfo;
+
+        public Transaction()
+        {
+        }
+        public Transaction(Account sender, Account receiver,int amount, string additionalInfo)
+        {
+            this.amount = amount;
+            this.additionalInfo = additionalInfo;
+        }
+
+        public void setTransaction()
+        {
+
+        }
+        public int Amount { get; set; }
+        public string AdditionalInfo { get; set; }
+
+
+        
+    }
+}
